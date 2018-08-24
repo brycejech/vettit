20bffbbfdc9dbfc1a3735b675e4b36e2c08096d8
Bryce Jech <bryce@brycejech.com>
2018-08-23 22:29:16 -0500

    add default states, statuses

--- src/install/sql/queries/state.sql
+++ src/install/sql/queries/state.sql
@@ -4,5 +4,16 @@ DROP TABLE IF EXISTS state CASCADE;
 CREATE TABLE state
 (
     id     serial       NOT NULL  PRIMARY KEY,
-    value  varchar(32)  NOT NULL  UNIQUE
+    name   varchar(32)  NOT NULL  UNIQUE
 );
+
+
+-- ADD DEFAULT STATES
+INSERT INTO state
+(name)
+
+VALUES
+('Open'),
+('Locked'),
+('Closed'),
+('Hidden');
--- src/install/sql/queries/status.sql
+++ src/install/sql/queries/status.sql
@@ -4,5 +4,19 @@ DROP TABLE IF EXISTS status CASCADE;
 CREATE TABLE status
 (
     id     serial       NOT NULL  PRIMARY KEY,
-    value  varchar(32)  NOT NULL  UNIQUE
+    name   varchar(32)  NOT NULL  UNIQUE
 );
+
+
+-- ADD DEFAULT STATUSES
+INSERT INTO status
+(name)
+
+VALUES
+('Queued'),
+('Open'),
+('Waiting'),
+('Pending Approval'),
+('Rejected'),
+('Stalled'),
+('Resolved'),
