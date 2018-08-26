40aa9bbe71b5e5774151b12838cad73cb363fef5
Bryce Jech <bryce@brycejech.com>
2018-08-25 23:34:40 -0500

    JOIN colors to tag SELECTs

--- src/ds/sql/queries/tag-get-all.sql
+++ src/ds/sql/queries/tag-get-all.sql
@@ -1,6 +1,17 @@
 
 SELECT
-    id, name, slug, org, color, created
+    t.id,
+    t.name,
+    t.slug,
+    c.name as color_name,
+    c.code as color_code,
+    t.org,
+    t.created
 
-FROM tag
+FROM tag t
+
+    JOIN
+        color c
+    ON
+        c.id=t.id
 ;
--- src/ds/sql/queries/tag-get-by-id.sql
+++ src/ds/sql/queries/tag-get-by-id.sql
@@ -1,8 +1,19 @@
 
 SELECT
-    id, name, slug, org, color, created
+    t.id,
+    t.name,
+    t.slug,
+    c.name as color_name,
+    c.code as color_code,
+    t.org,
+    t.created
 
-FROM tag
+FROM tag t
 
-WHERE id=$1
+    JOIN
+        color c
+    ON
+        c.id=t.id
+
+WHERE t.id=$1
 ;
