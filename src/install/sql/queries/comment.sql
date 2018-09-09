6d16ee6671e151d8637ff487e2380dcd00d6dcd7
Bryce Jech <bryce@brycejech.com>
2018-09-09 18:53:38 -0500

    remove parent_id col from comment table

--- src/install/sql/queries/comment.sql
+++ src/install/sql/queries/comment.sql
@@ -7,7 +7,6 @@ CREATE TABLE comment
     author       int        NOT NULL  REFERENCES account(id),
     body         text       NOT NULL,
     post_id      int        NOT NULL  REFERENCES post(id),
-    parent_id    int                  REFERENCES comment(id),
     modified_by  int                  REFERENCES account(id),
 
     created      timestamp with time zone DEFAULT (now() at time zone 'utc'),
